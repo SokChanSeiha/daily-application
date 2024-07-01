8023368dbbde1ed9e4bec1f30595b9d37f59e028
SokChanSeiha <sokchanseiha@gmail.com>
2024-07-01 17:43:09 +0700

    file: delete save button border

--- src/script.js
+++ src/script.js
@@ -1,7 +1,20 @@
 // (1) 
 let form = document.getElementById("form");
+// (2)
+let inputTitle = document.getElementById("title-input")
+
 
 form.addEventListener("submit", (e) => {
     console.log("clicked");
     e.preventDefault();
+    formValidation();
 });
+
+let formValidation = () => {
+    if(inputTitle.value === ""){
+        console.log("failure");
+    }
+    else{
+        console.log("success");
+    }
+}
