99bc985e86401e59f4c66e42c9f56e76cd16da7f
SokChanSeiha <sokchanseiha@gmail.com>
2024-07-02 23:59:21 +0700

    file: change font size

--- src/script.js
+++ src/script.js
@@ -4,15 +4,12 @@ const createNoteHTML = (id, title, description, date) => {
         <div class="Note" data-id="${id}">
           <div class="Note-header">
             <h2 class="Note-title">${title}</h2>
-            <!-- <h2 class="Note-title">New date</h2> -->
           </div>
           <div class="Note-body">
             <p class="Note-description">${description}</p>
-            <!-- <p class="Note-description">Wowowwwwww</p> -->
           </div>
           <div class="option">
             <span class="Note-date">${date}</span>
-            <!-- <span class="Note-date">2024-06-02</span> -->
             <div class="Note-footer">
               <button class="button read" onclick="window.location.href='./pages/read.html?id=${id}'">
                 <img class="read" src="./icon/view.svg" alt="Read icon" /></button>        
